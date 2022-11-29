3255b2eeaafbe47d716ddf7f35ca1cd0256ad91c
박병건 <byunggeon.park@sundaytoz.com>
2022-11-29 23:42:26 +0900

    드래그 터치

--- fighter/Assets/Scripts/InputController.cs
+++ fighter/Assets/Scripts/InputController.cs
@@ -6,10 +6,41 @@ namespace InGame
 {
     public class InputController : AbstractController
     {
+        private Vector2 clickPoint;
+        enum TouchDir
+        {
+            UP,
+            DOWN,
+            RIGHT,
+            LEFT
+        }
+        private TouchDir dir;
         public override void AdvancedTime(float inDeltaTime)
         {
             base.AdvancedTime(inDeltaTime);
+            if(Input.GetMouseButtonDown(0))
+            {
+                clickPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            }
 
+            if(Input.GetMouseButtonUp(0))
+            {
+                TouchDir dir = GetTouchDir(clickPoint, Input.mousePosition);
+            }
+        }
+
+        private TouchDir GetTouchDir(Vector2 inFrom, Vector2 inTo)
+        {
+            Vector2 dir = new Vector2(inTo.x - inFrom.x, inTo.y - inFrom.y);
+            if(dir.x < 0)
+            {
+                return TouchDir.LEFT;
+            }
+            if(dir.x >0)
+            {
+                return TouchDir.RIGHT;
+            }
+            return TouchDir.UP;
         }
     }
 }
