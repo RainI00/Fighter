76ff928d90f8ced415e79ce16101d11394d387c8
박병건 <byunggeon.park@sundaytoz.com>
2022-12-03 23:45:11 +0900

    카메라 move

--- fighter/Assets/Scripts/Camera/PlayerCamera.cs
+++ fighter/Assets/Scripts/Camera/PlayerCamera.cs
@@ -8,14 +8,16 @@ namespace InGame
     {
         [SerializeField] private Entity Target;
 
-        private Transform _tr;
         [SerializeField] private float Dist = 10f;
         [SerializeField] private float Height = 5f;
         [SerializeField] private float SmoothRotate = 5f;
+        private Transform _tr;
+        private Transform _trTarget;
         // Start is called before the first frame update
         void Start()
         {
             _tr = this.GetComponent<Transform>();
+            _trTarget = Target.transform;
         }
 
         // Update is called once per frame
@@ -26,12 +28,12 @@ namespace InGame
 
         private void LateUpdate()
         {
-            float currYAngle = Mathf.LerpAngle(_tr.eulerAngles.y, _tr.eulerAngles.y, SmoothRotate * Time.deltaTime);
+            float currYAngle = Mathf.LerpAngle(_tr.eulerAngles.y, _trTarget.eulerAngles.y, SmoothRotate * Time.deltaTime);
             Quaternion rot = Quaternion.Euler(0, currYAngle, 0);
 
-            _tr.position = Target.transform.position - (rot * Vector3.forward * Dist) + (Vector3.up * Height);
+            _tr.position = _trTarget.position - (rot * Vector3.forward * Dist) + (Vector3.up * Height);
 
-            _tr.LookAt(Target.transform);
+            _tr.LookAt(_trTarget);
         }
     }
 }
